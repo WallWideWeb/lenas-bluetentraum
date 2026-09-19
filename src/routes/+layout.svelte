64b262b6048fc51e79ee4d0c440d955cd6320873
tobiwall <t.w.95@web.de>
2026-09-19 22:24:38 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import './layout.css';
+	import { onMount } from 'svelte';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+
+	onMount(() => {
+		let cleanup = () => {};
+
+		(async () => {
+			const [{ default: Lenis }, { default: gsap }, { ScrollTrigger }] = await Promise.all([
+				import('lenis'),
+				import('gsap'),
+				import('gsap/ScrollTrigger')
+			]);
+
+			gsap.registerPlugin(ScrollTrigger);
+
+			const lenis = new Lenis({
+				duration: 1.2,
+				easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+				smoothWheel: true,
+				// Touch-Gesten bleiben nativ (kein Sync/"Hijacking"), damit Scrollen auf
+				// Mobilgeräten sich weiterhin natürlich und unverzögert anfühlt.
+				syncTouch: false,
+				touchMultiplier: 1
+			});
+
+			lenis.on('scroll', ScrollTrigger.update);
+
+			function raf(time: number) {
+				lenis.raf(time * 1000);
+			}
+
+			gsap.ticker.add(raf);
+			gsap.ticker.lagSmoothing(0);
+
+			cleanup = () => {
+				gsap.ticker.remove(raf);
+				lenis.destroy();
+			};
+		})();
+
+		return () => cleanup();
+	});
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
